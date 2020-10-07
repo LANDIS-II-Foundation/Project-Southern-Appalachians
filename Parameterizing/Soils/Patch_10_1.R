6f9e38ffa555471847ddb728cb84eb0d06eeb577
ZacharyRobbins <zjrobbin@NCSU.edu>
2020-10-07 11:44:05 -0400

    Updated SOM maps to reflect new parameterization
    New parameterization testing on single cells transformed into new C:N ratios and exchanges.

--- Parameterizing/Soils/Patch_10_1.R
+++ Parameterizing/Soils/Patch_10_1.R
@@ -1,21 +1,21 @@
 library(raster)
 Drive<-'C:/Users/zacha/Documents/GitHub/Project-Southern-Appalachians-2018/Parameterizing/Soils/'
 Surf<-raster::raster(paste0(Drive,"MRSOM1surfCmap.tif"))
-Surf2C<-Surf*.07                     
-plot(Surf2,zlim=c(0,5))
-SurfN<-Surf2/100
-plot(SurfN,zlim=c(0,.1))
+plot(Surf,zlim=c(0,100))
+SurfN<-Surf/10
+plot(SurfN,zlim=c(0,10))
 
 Soil<-raster::raster(paste0(Drive,"MRSOM1SoilCmap.tif"))
-SoilN<-Soil/8
+SoilN<-Soil/10
 plot(SoilN,zlim=c(0,12))
 
-Soil2<-raster::raster(paste0(Drive,"MRSOM2SoilCmap.tif"))
+Soil2<-raster::raster(paste0(Drive,"MRSOM2Cmap.tif"))
+Soil2N<-Soil2/17.5
 
 
 
 
+writeRaster(SoilN,paste0(Drive,"SOM1soilNmap_v1_1.TIF"),overwrite=T)
+writeRaster(SurfN,paste0(Drive,'SOM1surfNmap_v1_1.TIF'),overwrite=T)
+writeRaster(Soil2N,paste0(Drive,'SOM2Nmap_v1_1.TIF'),overwrite=T)
 
-writeRaster(SoilN,paste0(Drive,"SOM1soilNmap_v1_1.tif"))
-writeRaster(SurfN,paste0(Drive,'SOM1surfNmap_v1_1.tif'))
-writeRaster(Surf2C,paste0(Drive,'SOM1surfCmap_v1_1.tif'))
